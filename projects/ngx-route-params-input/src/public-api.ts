98b181191f20ff0496e1512c0caeb06ff461aecd
Andrii Cherepovskyi <nofamous.ru@gmail.com>
2020-04-25 12:12:30 +0300

    Hotfix 1.0.1: Fixed logic of old input params determination

--- projects/ngx-route-params-input/src/public-api.ts
+++ projects/ngx-route-params-input/src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of ngx-route-params-input
  */
 
-export * from './lib/ngx-route-params-input.service';
 export * from './lib/ngx-route-params-input.component';
 export * from './lib/ngx-route-params-input.module';
 export * from './lib/get-all-route-params';
