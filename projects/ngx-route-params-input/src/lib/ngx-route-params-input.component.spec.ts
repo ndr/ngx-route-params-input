8b1aaa42b033ca3cdabd7a9a3a4106dc90cc8474
Andrii Cherepovskyi <nofamous.ru@gmail.com>
2020-11-23 23:14:44 +0200

    Added ng 10 and ng 11 support

--- projects/ngx-route-params-input/src/lib/ngx-route-params-input.component.spec.ts
+++ projects/ngx-route-params-input/src/lib/ngx-route-params-input.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { NgxRouteParamsInputComponent } from './ngx-route-params-input.component';
 
@@ -6,7 +6,7 @@ describe('NgxRouteParamsInputComponent', () => {
   let component: NgxRouteParamsInputComponent;
   let fixture: ComponentFixture<NgxRouteParamsInputComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NgxRouteParamsInputComponent ]
     })
